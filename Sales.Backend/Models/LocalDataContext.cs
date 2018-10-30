36e5bc14b33570923270f74cd2fff74f671232aa
estebans <esgomez48@gmail.com>
2018-10-30 16:54:57 -0400

    project creation

---
+++ Sales.Backend/Models/LocalDataContext.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace Sales.Backend.Models
+{
+    using Sales.Domain.Models;
+    //esto se hace para facilitar las migraciones de datos
+    public class LocalDataContext : DataContext
+    {
+        public System.Data.Entity.DbSet<Sales.Common.Models.Product> Products { get; set; }
+    }
+}
---
+++ Sales.Common/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿
+// es un error pensar q los modelos son tablas, solo algunos...solo van a base de datos los que vamos a persisit
+namespace Sales.Common.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public bool IsAvailable { get; set; }
+
+        public DateTime PublishedOn { get; set; }
+    }
+}
